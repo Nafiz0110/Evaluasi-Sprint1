72ae0e959df954720a5e0d48b0b66a8a278840b5
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-24 09:53:27 +0700

    Jawaban Soal Nomer (1)

---
+++ Nomer 1/Jawaban no 1.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+    <title>Evaluasi SPRINT1 (Soal2)</title>
+</head>
+<body>
+<script>
+	alert("Nafizul Izzata Insan Nakaya\nMobile (1)")
+    var nilai = prompt("Masukan Nilai Yang Anda Dapatkan:");
+    var grade = "";
+
+    if (nilai >= 90){
+        grade = "(A)"
+      document.write("<h2>" + 'Grade : '+ grade + "<p>")
+    } else if(nilai >= 80) {
+        grade = "(B+)"
+       document.write("<h2>" + 'Grade : '+ grade + "<p>")
+    } else if(nilai >= 70) {
+        rangking = "(B)"
+       document.write("<h2>" + 'Grade : '+ grade + "<p>")
+       }
+</script>
+</body>
+</html>
