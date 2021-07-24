eaa5d48c25362942941a7c2f8ea7737f25a6dfd0
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-24 10:50:03 +0700

    Jawaban Soal Nomer (5)

---
+++ Nomer 5/Jawaban no 5.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Evaluasi Sprint1</title>
+</head>
+<body>
+    <script>
+    alert("Nafizul Izzata Insan Nakaya\nMobile (1)")
+
+var phi : float;
+var r, luas;integer;
+phi <-- 3.14;
+
+read(r)
+luas <-- phi * r *r;
+write(luas)
+</script>
+</body>
+</html>
