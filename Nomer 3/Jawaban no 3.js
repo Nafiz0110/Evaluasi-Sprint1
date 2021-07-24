7b1b5de811907db475c44f519776ab45118acfed
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-24 09:56:41 +0700

    Jawaban Soal Nomer (3)

---
+++ Nomer 3/Jawaban no 3.js
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Percabangan switch/case</title>
+</head>
+<body>
+    <script>
+	alert("Nafizul Izzata Insan Nakaya\nMobile (1)")
+for(var a = 1; a < 10; a++) {
+    for(var b = 1; b <10; b++) {
+         if(a == b || a + b == 10) {
+             document.write(' * ')
+         } else {
+             document.write(' _ ')
+         }
+    }
+    document.write('<br>');
+}
+</script>
+</body>
+</html>
