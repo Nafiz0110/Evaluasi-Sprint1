f8bc44b5b0c5f14ad0947f18cc68358353b76ebe
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-24 10:09:17 +0700

    Add files via upload

---
+++ Nomer 4/Jawaban no 4.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Nafizz</title>
+</head>
+<body>
+    <script>
+for(let i = 60; i < 100; i++){
+    if(i%2==0) {
+    document.write("<p>Angka Genap : " + i + "</p>")
+}
+}
+</script>
+</body>
+</html>
