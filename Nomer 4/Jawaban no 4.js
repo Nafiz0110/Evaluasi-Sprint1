c7e352a78489262ffb7534c61fe335e005ec04c5
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-24 10:15:11 +0700

    Jawaban Soal Nomer (4)

--- Nomer 4/Jawaban no 4.js
+++ Nomer 4/Jawaban no 4.js
@@ -5,6 +5,7 @@
 </head>
 <body>
     <script>
+    alert("Nafizul Izzata Insan Nakaya\nMobile (1)")
 for(let i = 60; i < 100; i++){
     if(i%2==0) {
     document.write("<p>Angka Genap : " + i + "</p>")
